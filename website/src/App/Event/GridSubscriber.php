9b683929d07ef5725bd39fed342eadfbcd6f1c56
Laurent Minguet <laurent@spareka.com>
2019-10-25 11:34:06 +0200

    add global actions on grid + better std = better php unit tests

--- website/src/App/Event/GridSubscriber.php
+++ website/src/App/Event/GridSubscriber.php
@@ -28,10 +28,19 @@ class GridSubscriber implements EventSubscriberInterface
      */
     public function onGrid(GridDefinitionEvent $event): void
     {
-        $event->getGridDefinition()->addColumn(
-            (new Grid\Column('middle_name', 'spipu.user.field.middle_name', 'middleName', 35))
-                    ->setType((new Grid\ColumnType(Grid\ColumnType::TYPE_TEXT)))
-                    ->useSortable()
-        );
+        $grid = $event->getGridDefinition();
+
+        $grid
+            ->addColumn(
+                (new Grid\Column('middle_name', 'spipu.user.field.middle_name', 'middleName', 35))
+                        ->setType((new Grid\ColumnType(Grid\ColumnType::TYPE_TEXT)))
+                        ->useSortable()
+            )
+            ->addGlobalAction(
+                (new Grid\Action('create', 'spipu.ui.action.create', 10, 'spipu_user_admin_create'))
+                    ->setIcon('edit')
+                    ->setNeededRole('ROLE_ADMIN_MANAGE_USER_EDIT')
+                    ->setCssClass('success')
+            );
     }
 }
