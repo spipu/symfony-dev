021c3ae539a15c199719db5ce06db067f17b50ed
Laurent Minguet <laurent@spareka.com>
2019-11-27 10:59:10 +0100

    update symfony to 4.4 fix unit tests for symfony mailer fix issue on some repositories fix issue on deprecated doctrine bundle usage

--- website/src/App/Repository/UserRepository.php
+++ website/src/App/Repository/UserRepository.php
@@ -4,15 +4,15 @@ declare(strict_types = 1);
 namespace App\Repository;
 
 use App\Entity\User;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 class UserRepository extends \Spipu\UserBundle\Repository\UserRepository
 {
     /**
      * UserRepository constructor.
-     * @param RegistryInterface $registry
+     * @param ManagerRegistry $registry
      */
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
     }
