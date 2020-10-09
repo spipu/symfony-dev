4960ae03406219492d1c1e3103845f15abaa8d3a
Laurent Minguet <laurent@spareka.com>
2020-10-09 14:39:37 +0200

    upgrade to symfony 4.4.15

---
+++ website/src/App/.preload.php
@@ -0,0 +1,5 @@
+<?php
+
+if (file_exists(__DIR__.'/../../var/cache/dev/AppApp_KernelDevDebugContainer.preload.php')) {
+    require __DIR__.'/../../var/cache/dev/AppApp_KernelDevDebugContainer.preload.php';
+}
