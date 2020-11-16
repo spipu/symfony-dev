b3b34087340b59b439f6dd6b07fcf6f5d79c75cc
Laurent Minguet <laurent@spareka.com>
2020-11-16 17:24:16 +0100

    add password configuration type + prepare encrypted configuration type

--- website/src/App/Controller/MainController.php
+++ website/src/App/Controller/MainController.php
@@ -3,6 +3,8 @@ declare(strict_types = 1);
 
 namespace App\Controller;
 
+use Spipu\ConfigurationBundle\Exception\ConfigurationException;
+use Spipu\ConfigurationBundle\Service\Manager as ConfigurationManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,10 +17,22 @@ class MainController extends AbstractController
 {
     /**
      * @Route("/", name="app_home", methods="GET")
+     * @param ConfigurationManager $configurationManager
      * @return Response
+     * @throws ConfigurationException
      */
-    public function home(): Response
-    {
-        return $this->render('main/home.html.twig');
+    public function home(
+        ConfigurationManager $configurationManager
+    ): Response {
+        return $this->render(
+            'main/home.html.twig',
+            [
+                'configuration' => [
+                    'goodPassword' => $configurationManager->isPasswordValid('test.type.password', 'password'),
+                    'encrypted'    => $configurationManager->get('test.type.encrypted'),
+                    'decrypted'    => $configurationManager->getEncrypted('test.type.encrypted'),
+                ]
+            ]
+        );
     }
 }
