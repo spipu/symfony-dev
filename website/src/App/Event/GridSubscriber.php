43c99c7a922fae4f92209e1f3d0529a223827b17
spipu <spipu45@gmail.com>
2022-04-09 23:46:15 +0200

    can hide a column but keep the filter

--- website/src/App/Event/GridSubscriber.php
+++ website/src/App/Event/GridSubscriber.php
@@ -47,6 +47,8 @@ class GridSubscriber implements EventSubscriberInterface
                 (new Grid\Column('middle_name', 'spipu.user.field.middle_name', 'middleName', 35))
                         ->setType((new Grid\ColumnType(Grid\ColumnType::TYPE_TEXT)))
                         ->useSortable()
+                        ->setFilter((new Grid\ColumnFilter(true, false)))
+                        ->setDisplayed(false)
             )
             ->addGlobalAction(
                 (new Grid\Action('create', 'spipu.ui.action.create', 10, 'spipu_user_admin_create'))
