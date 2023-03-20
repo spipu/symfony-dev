c955b28ffbcbc56380c61d173236e8352660b712
Laurent Minguet <laurent@spareka.com>
2023-03-20 16:25:45 +0100

    use the new dashboard bundle

---
+++ website/src/App/WidgetSource/UserNb.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\WidgetSource;
+
+use Spipu\DashboardBundle\Entity\Source as Source;
+
+class UserNb extends AbstractSource
+{
+    /**
+     * @return Source\SourceSql
+     */
+    public function getDefinition(): Source\SourceSql
+    {
+        return (new Source\SourceSql("user-count", 'spipu_user'))
+            ->setDateField(null);
+    }
+}
