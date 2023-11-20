04a55cff90b7356d73255325765466d75c109da1
spipu <spipu45@gmail.com>
2023-11-20 22:29:23 +0100

    add new data provider and source type

--- website/src/App/WidgetSource/Test.php
+++ website/src/App/WidgetSource/Test.php
@@ -5,21 +5,25 @@ declare(strict_types=1);
 namespace App\WidgetSource;
 
 use Spipu\ConfigurationBundle\Form\Options\ScopeOptions;
+use Spipu\ConfigurationBundle\Service\ConfigurationManager;
 use Spipu\DashboardBundle\Entity\Source as Source;
+use Spipu\DashboardBundle\Service\Ui\Widget\WidgetRequest;
+use Spipu\DashboardBundle\Source\SourceDataDefinitionInterface;
 
-class Test extends AbstractSource
+class Test extends AbstractSource implements SourceDataDefinitionInterface
 {
     private ScopeOptions $scopeOptions;
+    private ConfigurationManager $configurationManager;
 
-    public function __construct(ScopeOptions $scopeOptions)
+    public function __construct(ScopeOptions $scopeOptions, ConfigurationManager $configurationManager)
     {
         $this->scopeOptions = $scopeOptions;
+        $this->configurationManager = $configurationManager;
     }
 
-    public function getDefinition(): Source\SourceSql
+    public function getDefinition(): Source\SourceFromDefinition
     {
-        return (new Source\SourceSql("test", ''))
-            ->setDateField(null)
+        return (new Source\SourceFromDefinition("test", $this))
             ->setSpecificDisplay('spider', 'dashboard/widget/test.html.twig')
             ->addFilter(
                 new Source\SourceFilter(
@@ -29,5 +33,30 @@ class Test extends AbstractSource
                     $this->scopeOptions
                 )
             );
+    }
+
+    public function getValue(WidgetRequest $request): float
+    {
+        return 0.;
+    }
+
+    public function getPreviousValue(WidgetRequest $request): float
+    {
+        return 0.;
+    }
+
+    public function getValues(WidgetRequest $request): array
+    {
+        return [];
+    }
+
+    public function getSpecificValues(WidgetRequest $request): array
+    {
+        $scope = $request->getFilterValueString('scope');
+
+        return [
+            'conf' => $this->configurationManager->get('test.type.text', $scope),
+            'scope' => $scope,
+        ];
     }
 }
