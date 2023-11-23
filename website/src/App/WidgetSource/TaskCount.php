5031dbd19d57654867a051292f35fdd9406a05cb
Laurent Minguet <laurent@spareka.com>
2023-11-23 13:12:56 +0100

    fix bad period name + add translated filters

--- website/src/App/WidgetSource/TaskCount.php
+++ website/src/App/WidgetSource/TaskCount.php
@@ -5,11 +5,28 @@ declare(strict_types=1);
 namespace App\WidgetSource;
 
 use Spipu\DashboardBundle\Entity\Source as Source;
+use Spipu\ProcessBundle\Form\Options\Status;
 
 class TaskCount extends AbstractSource
 {
+    private Status $statusOptions;
+
+    public function __construct(Status $statusOptions)
+    {
+        $this->statusOptions = $statusOptions;
+    }
+
     public function getDefinition(): Source\SourceSql
     {
-        return (new Source\SourceSql("task-count", 'spipu_process_task'));
+        return (new Source\SourceSql("task-count", 'spipu_process_task'))
+            ->addFilter(
+                new Source\SourceFilter(
+                    'status',
+                    'spipu.configuration.scope.label',
+                    'status',
+                    $this->statusOptions,
+                    true
+                )
+            );
     }
 }
